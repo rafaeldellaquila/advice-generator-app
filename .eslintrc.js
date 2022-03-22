c4da89f1b4667677b00938d3fc90cd208efafd57
Rafael Dell'Aquila <rafael@dellaquila.dev>
2022-03-22 13:30:07 -0300

    :wrench: prettier, eslint, package, babel, wbpkg

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,33 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'max-len': [
+            'error',
+            {
+                code: 150,
+                ignoreUrls: true,
+            },
+        ],
+
+        'prettier/prettier': [
+            'error',
+            {},
+            {
+                usePrettierrc: true,
+            },
+        ],
+    },
+}
